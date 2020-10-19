88421d7e279e114089bc20bf252fd195561ea000
justablob <me@blob.dev>
2020-10-19 11:52:29 +0200

    Improve password hashing functions

--- src/utils/password.ts
+++ src/utils/password.ts
@@ -1,5 +1,24 @@
 import crypto from "crypto";
 
+const ALGOS = {
+  "sha256": {
+    keylen: 32
+  },
+  "sha384": {
+    keylen: 48
+  },
+  "sha512": {
+    keylen: 64
+  },
+  "blake2b512": {
+    keylen: 64,
+  },
+};
+
+const DEFAULT_SALT_LENGTH = 16;
+const DEFAULT_ALGO: keyof typeof ALGOS = "blake2b512";
+const DEFAULT_ROUNDS = 32000;
+
 function split (hashed: string) {
   let splitHash = hashed.split("$").slice(1, -1);
   let meta = splitHash[0].split(":");
@@ -23,25 +42,6 @@ function join ({ salt, hash, algo, rounds }: { salt: Buffer, hash: Buffer, algo:
   return `$${meta}$${data}$`;
 }
 
-const ALGOS = {
-  "sha256": {
-    keylen: 32
-  },
-  "sha384": {
-    keylen: 48
-  },
-  "sha512": {
-    keylen: 64
-  },
-  "blake2b512": {
-    keylen: 64,
-  },
-};
-
-const DEFAULT_SALT_LENGTH = 18;
-const DEFAULT_ALGO = "blake2b512";
-const DEFAULT_ROUNDS = 32000;
-
 export function hash (password: string): Promise<string> {
   return new Promise((res, rej) => {
     let salt = crypto.randomBytes(DEFAULT_SALT_LENGTH);
@@ -49,6 +49,7 @@ export function hash (password: string): Promise<string> {
     let rounds = DEFAULT_ROUNDS;
     let keylen: number = ALGOS[algo].keylen;
 
+    if (crypto.getHashes().indexOf(algo) === -1) throw new Error("Invalid hashing algorithm");
     return crypto.pbkdf2(password, salt, rounds, keylen, algo, (err, hash) => {
       if (err) return rej(err);
       return res(join({
@@ -64,8 +65,10 @@ export function hash (password: string): Promise<string> {
 export function verify (password: string, hashed: string): Promise<boolean> {
   return new Promise((res, rej) => {
     let { algo, rounds, salt, hash } = split(hashed);
+    if (!ALGOS[algo]) throw new Error("Invalid algorithm identifier");
     let keylen: number = ALGOS[algo].keylen;
 
+    if (crypto.getHashes().indexOf(algo) === -1) throw new Error("Invalid hashing algorithm");
     return crypto.pbkdf2(password, salt, rounds, keylen, algo, (err, computedHash) => {
       if (err) return rej(err);
       return res(crypto.timingSafeEqual(hash, computedHash));
